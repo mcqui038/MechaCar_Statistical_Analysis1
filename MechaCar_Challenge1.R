61c901aa78f0832d8a1d41b4dfe29f1852424df6
Kelly McQuillan <kellymcquillan@Kellys-MacBook-Air.local>
2022-04-10 21:50:52 -0500

    Finished R script for deliverable 2

---
+++ MechaCar_Challenge1.R
@@ -0,0 +1,9 @@
+library(dplyr)
+# Deliverable 1
+MechaCar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data= MechaCar_mpg_df)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data= MechaCar_mpg_df))
+#Deliverable 2
+Suspension_Coil_DF <- read.csv(file="Suspension_Coil.csv",check.names=F,stringsAsFactors = F)
+total_summary <- Suspension_Coil_DF %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), STD_PSI=sd(PSI))
+lot_summary <- Suspension_Coil_DF %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), STD_PSI=sd(PSI))
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ### Linear Regression to Predict MPG
 Ground Clearance is the only variable that provided a non-random amount of variance to the mpg values in the dataset. The slope of the line is not zero because the r-squared value is greater than zero. This model does not predict the mpg of MechaCar prototypes well because only one of the variables was significant. 
+
+### Summary Statistics on Suspension Coils
